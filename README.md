613a205f0bc2996b8cf68b424b3344354d4778d2
Tafara7 <hwatatafara4@gmail.com>
2025-10-30 03:40:48 +0200

    additional config commits

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This is a simple version control website focused on code that allows users to share and update files in shared projects, view other projects, add friends, collaborate, etc.
 
+All relevant files are in their respective folders (frontend and backend).
+
 # To build and run the app in Docker:
 docker build -t nebula-app .
 docker run -p 3000:3000 nebula-app
--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -3,8 +3,8 @@ import Header from "../components/Header";
 import SignupForm from "../components/SignupForm";
 
 const SignupPage = () => {
-  return (
-    <div className="signup-page">
+  return ( 
+    <div className="signup-page flex items-center justify-between p-4 bg-nebula-dark rounded-lg">
       <Header />
       <main className="main-content">
         <h1>Sign Up for NebulaCode</h1>
